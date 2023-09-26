e71909bdc0005fe8bf9bb349c74bdc0f2429bf33
Anisha0908 <144648815+Anisha0908@users.noreply.github.com>
2023-09-26 11:10:25 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,22 +1,13 @@
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-    return sorted_students
 
-# Define a student class
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
-
-# Test the function with sample student objects
-student1 = Student("Alice", "S001", 3.8)
-student2 = Student("Bob", "S002", 3.9)
-student3 = Student("Charlie", "S003", 3.7)
-
-students = [student1, student2, student3]
-
-sorted_students = sort_students(students)
-
-for student in sorted_students:
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+#Enter input
+n = int(input("Enter input number : "))
+ 
+fact = 1
+if n < 0:
+    print("Factorial does not exist for negative numbers")
+elif n == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, n + 1):
+        fact = fact * i
+    print("The factorial of",n,"is",fact)
